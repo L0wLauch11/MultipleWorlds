58cc92ab46f27ed1e53c711a02926fcc46d6f2a0
L0wLauch11 <42972587+L0wLauch11@users.noreply.github.com>
2021-04-28 00:58:53 +0200

    Permissions to switch
    added better permissions for allowing which player should be able to switch to certain worlds

--- src/main/java/me/lowlauch/multipleworlds/Commands.java
+++ src/main/java/me/lowlauch/multipleworlds/Commands.java
@@ -10,6 +10,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class Commands implements CommandExecutor
 {
@@ -59,13 +60,15 @@ public class Commands implements CommandExecutor
     public boolean onCommand(CommandSender commandSender, Command command, String commandLabel, String[] args)
     {
         // Admin command
-        if(commandLabel.equalsIgnoreCase("mw") && commandSender.isOp())
+        boolean permission = commandSender.isOp() || commandSender.hasPermission("multipleworlds.admin");
+        if(commandLabel.equalsIgnoreCase("mw") && permission)
         {
-            // Automates creating a multiple new worlds
+            // Creates a new "dimension"
             if(args[0].equalsIgnoreCase("create"))
             {
                 if(args.length == 2)
                 {
+                    // Create overworld, nether and the end
                     Bukkit.getServer().dispatchCommand(commandSender, "mv create " + args[1]);
                     Bukkit.getServer().dispatchCommand(commandSender, "mv create " + args[1] + "_nether");
                     Bukkit.getServer().dispatchCommand(commandSender, "mv create " + args[1] + "_the_end");
@@ -73,8 +76,106 @@ public class Commands implements CommandExecutor
                     // Workaround for multiverse end portals not working
                     Bukkit.getServer().dispatchCommand(commandSender, "mv modify set respawnWorld " + args[1] + " " + args[1] + "_the_end");
 
+                    // Set right respawnworld for nether
+                    Bukkit.getServer().dispatchCommand(commandSender, "mvm set respawnworld " + args[1] + " " + args[1] + "_nether");
+
+                    // Allow this dimension to be switchable
+                    Main.getInstance().getConfig().set("mw." + args[1] + ".switchable", true);
+
+                    // Only allow people with permission to switch to this world
+                    Main.getInstance().getConfig().set("mw." + args[1] + ".everyone", false);
+
+                    // Allow only server version per default
+                    Main.getInstance().getConfig().set("mw." + args[1] + ".allowedversions", Bukkit.getVersion());
+
+                    Main.getInstance().getConfig().set("mw." + args[1] + ".allowedplayers", "a");
+
+                    Main.getInstance().saveConfig();
+
                 } else
                     commandSender.sendMessage(ChatColor.RED + "You have to put in a world name!");
+
+                return true;
+            }
+
+            // Allows everyone or disallows everyone to switch to a specific world
+            if(args[0].equalsIgnoreCase("everyone"))
+            {
+                if(args.length == 3)
+                {
+                    String world = args[1];
+
+                    if(args[2].equalsIgnoreCase("true") || args[2].equalsIgnoreCase("false"))
+                    {
+                        Main.getInstance().getConfig().set("mw." + world + ".everyone", Boolean.parseBoolean(args[2]));
+                        Main.getInstance().saveConfig();
+                        commandSender.sendMessage(ChatColor.GREEN + "Sucessfully changed value to " + ChatColor.GOLD + args[2]);
+                    }
+                } else
+                    commandSender.sendMessage(ChatColor.RED + "Provide a world and \"true\" or \"false\"");
+
+                return true;
+            }
+
+            // Enables or disables switching to a specific world
+            if(args[0].equalsIgnoreCase("switchable"))
+            {
+                if(args.length == 3)
+                {
+                    String world = args[1];
+
+                    if(args[2].equalsIgnoreCase("true") || args[2].equalsIgnoreCase("false"))
+                    {
+                        Main.getInstance().getConfig().set("mw." + world + ".switchable", Boolean.parseBoolean(args[1]));
+                        Main.getInstance().saveConfig();
+                        commandSender.sendMessage(ChatColor.GREEN + "Sucessfully changed value to " + ChatColor.GOLD + args[2]);
+                    }
+                } else
+                    commandSender.sendMessage(ChatColor.RED + "Provide a world and \"true\" or \"false\"");
+
+                return true;
+            }
+
+            // Allows/Disallows a certain player to switch to a world
+            if(args[0].equalsIgnoreCase("allowplayer"))
+            {
+                if(args.length == 4)
+                {
+                    String world = args[1];
+
+                    String playerUUID;
+                    if(Bukkit.getPlayer(args[3]) == null)
+                        playerUUID = Bukkit.getOfflinePlayer(args[3]).getUniqueId().toString();
+                    else
+                        playerUUID = Objects.requireNonNull(Bukkit.getPlayer(args[3])).getUniqueId().toString();
+
+                    if(args[2].equalsIgnoreCase("true") || args[2].equalsIgnoreCase("false"))
+                    {
+                        String allowedPlayers = Main.getInstance().getConfig().getString("mw." + world + ".allowedplayers");
+
+                        // Allow or disallow
+                        boolean allow = Boolean.parseBoolean(args[2]);
+                        assert allowedPlayers != null;
+                        if(allow)
+                        {
+                            if(!allowedPlayers.contains(playerUUID))
+                                Main.getInstance().getConfig().set("mw." + world + ".allowedplayers", allowedPlayers + " " + playerUUID);
+                        }
+                        else
+                        {
+                            Main.getInstance().getConfig().set("mw." + world + ".allowedplayers", allowedPlayers.replaceAll(playerUUID, ""));
+                        }
+
+                        Main.getInstance().saveConfig();
+
+                        String s = args[2].replaceAll("true", "§aallowed ");
+                        s = s.replaceAll("false", "§cdisallowed ");
+
+                        commandSender.sendMessage(ChatColor.GREEN + "Sucessfully " + s + ChatColor.GOLD + playerUUID
+                                + " to switch to world " + ChatColor.GOLD + world);
+                    }
+                } else
+                    commandSender.sendMessage(ChatColor.RED + "Provide a world, \"true\" or \"false\" and a player name");
 
                 return true;
             }
@@ -85,86 +186,102 @@ public class Commands implements CommandExecutor
         {
             Player p = (Player) commandSender;
 
-            // Args have to be at least once
-            if(p.hasPermission("multipleworlds.switch") && args.length >= 1
-                    && !p.getWorld().getName().endsWith("_nether")
-                    && !p.getWorld().getName().endsWith("_end"))
+            if(args.length >= 1)
             {
-                savePlayerData(p);
+                // Args have to be at least once
+                permission = Objects.requireNonNull(Main.getInstance().getConfig().getString("mw." + args[0] + ".allowedplayers")).contains(p.getUniqueId().toString())
+                        || Main.getInstance().getConfig().getBoolean("mw." + args[0] + ".everyone");
 
-                // Switch world
-                // Load the data
-                String uuid = p.getUniqueId().toString();
-                String loadPath = uuid + "." + args[0];
-                String destInventoryString = Main.getInstance().getConfig().getString(loadPath + ".inventory");
-
-                if(destInventoryString != null) {
-                    String destArmorString = Main.getInstance().getConfig().getString(loadPath + ".armor");
-                    String destEnderChestString = Main.getInstance().getConfig().getString(loadPath + ".enderchest");
-                    double destXp = Main.getInstance().getConfig().getDouble(loadPath + ".xp");
-                    int destLevel = Main.getInstance().getConfig().getInt(loadPath + ".level");
-                    double destHealth = Main.getInstance().getConfig().getDouble(loadPath + ".health");
-                    double destSaturation = Main.getInstance().getConfig().getDouble(loadPath + ".sat");
-                    double destFood = Main.getInstance().getConfig().getDouble(loadPath + ".food");
-
-                    Location destLoc = Main.getInstance().getConfig().getLocation(loadPath + ".location");
-                    Location destSpawnworldloc = Main.getInstance().getConfig().getLocation(loadPath + ".bedspawn");
-                    Location fallbackLoc = new Location(Bukkit.getServer().getWorld(args[0]), 0, 60, 0);
-
-                    if(destLoc != null)
-                    {
-                        p.teleport(destLoc);
-                    }
-                    else
-                    {
-                        p.sendMessage(ChatColor.RED + "Your last coordinates could not be found. You will be teleported to 0, 0");
-                        p.teleport(fallbackLoc);
-                    }
-
-                    if(destSpawnworldloc != null)
-                        p.setBedSpawnLocation(destSpawnworldloc, true);
-                    else
-                        p.setBedSpawnLocation(fallbackLoc, true);
-
-                    p.setLevel(destLevel);
-                    p.setExp((float) destXp);
-
-                    // Prevent the player from dying instantly
-                    if(destHealth != 0)
-                    {
-                        p.setHealth(destHealth);
-                        p.setSaturation((float) destSaturation);
-                        p.setFoodLevel((int) destFood);
-                    }
-
-                    p.getInventory().clear();
-
-                    try {
-                        p.getInventory().setContents(StringInventory.itemStackArrayFromBase64(destInventoryString));
-                        p.getInventory().setArmorContents(StringInventory.itemStackArrayFromBase64(destArmorString));
-                        p.getEnderChest().setContents(StringInventory.itemStackArrayFromBase64(destEnderChestString));
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                    p.getInventory().setItemInOffHand(Main.getInstance().getConfig().getItemStack(loadPath + ".offhand"));
-
-
-                    p.updateInventory();
-                } else
+                // Check if world is even switchable
+                if(!Main.getInstance().getConfig().getBoolean("mw." + args[0] + ".switchable"))
                 {
-                    Location destLocation = new Location(Bukkit.getServer().getWorld(args[0]), 0, 60, 0);
-                    p.teleport(destLocation);
-                    p.setBedSpawnLocation(destLocation, true);
-                    p.setLevel(0);
-                    p.setExp(0f);
-
-                    p.getInventory().clear();
-                    p.updateInventory();
-                    savePlayerData(p);
+                    commandSender.sendMessage(ChatColor.RED + "This world is not switchable!");
+                    return true;
                 }
 
-                commandSender.sendMessage("§7Welcome to §6" + args[0] + "§7!");
-                return true;
+                // Switch world
+                if(permission && !p.getWorld().getName().endsWith("_nether") && !p.getWorld().getName().endsWith("_end"))
+                {
+                    savePlayerData(p);
+
+                    // Switch world
+                    // Load the data
+                    String uuid = p.getUniqueId().toString();
+                    String loadPath = uuid + "." + args[0];
+                    String destInventoryString = Main.getInstance().getConfig().getString(loadPath + ".inventory");
+
+                    if(destInventoryString != null)
+                    {
+                        String destArmorString = Main.getInstance().getConfig().getString(loadPath + ".armor");
+                        String destEnderChestString = Main.getInstance().getConfig().getString(loadPath + ".enderchest");
+                        double destXp = Main.getInstance().getConfig().getDouble(loadPath + ".xp");
+                        int destLevel = Main.getInstance().getConfig().getInt(loadPath + ".level");
+                        double destHealth = Main.getInstance().getConfig().getDouble(loadPath + ".health");
+                        double destSaturation = Main.getInstance().getConfig().getDouble(loadPath + ".sat");
+                        double destFood = Main.getInstance().getConfig().getDouble(loadPath + ".food");
+
+                        Location destLoc = Main.getInstance().getConfig().getLocation(loadPath + ".location");
+                        Location destSpawnworldloc = Main.getInstance().getConfig().getLocation(loadPath + ".bedspawn");
+                        Location fallbackLoc = new Location(Bukkit.getServer().getWorld(args[0]), 0, 60, 0);
+
+                        if(destLoc != null)
+                        {
+                            p.teleport(destLoc);
+                        }
+                        else
+                        {
+                            p.sendMessage(ChatColor.RED + "Your last coordinates could not be found. You will be teleported to 0, 0");
+                            p.teleport(fallbackLoc);
+                        }
+
+                        if(destSpawnworldloc != null)
+                            p.setBedSpawnLocation(destSpawnworldloc, true);
+                        else
+                            p.setBedSpawnLocation(fallbackLoc, true);
+
+                        p.setLevel(destLevel);
+                        p.setExp((float) destXp);
+
+                        // Prevent the player from dying instantly
+                        if(destHealth != 0)
+                        {
+                            p.setHealth(destHealth);
+                            p.setSaturation((float) destSaturation);
+                            p.setFoodLevel((int) destFood);
+                        }
+
+                        p.getInventory().clear();
+
+                        try {
+                            p.getInventory().setContents(StringInventory.itemStackArrayFromBase64(destInventoryString));
+                            p.getInventory().setArmorContents(StringInventory.itemStackArrayFromBase64(destArmorString));
+                            p.getEnderChest().setContents(StringInventory.itemStackArrayFromBase64(destEnderChestString));
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        p.getInventory().setItemInOffHand(Main.getInstance().getConfig().getItemStack(loadPath + ".offhand"));
+
+
+                        p.updateInventory();
+                    } else
+                    {
+                        Location destLocation = new Location(Bukkit.getServer().getWorld(args[0]), 0, 60, 0);
+                        p.teleport(destLocation);
+                        p.setBedSpawnLocation(destLocation, true);
+                        p.setLevel(0);
+                        p.setExp(0f);
+
+                        p.getInventory().clear();
+                        p.updateInventory();
+                        savePlayerData(p);
+                    }
+
+                    commandSender.sendMessage("§7Welcome to §6" + args[0] + "§7!");
+                    return true;
+                } else
+                {
+                    commandSender.sendMessage(ChatColor.RED + "You do not have permission to travel to this world!");
+                }
             }
         }
 
